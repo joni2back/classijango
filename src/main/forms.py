1d1089761f3d45f09ab11796e8206b64968c6265
joni2back <joni2back@gmail.com>
2013-10-03 14:57:23 -0300

    beta article edition, related city entity to classified

--- src/main/forms.py
+++ src/main/forms.py
@@ -8,8 +8,8 @@ from main.models.locations import Country, Province, City
 
 
 class AddClassifiedForm(ModelForm):
-    contact_city = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'autocomplete': 'off'}))
-    contact_city_id = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'type': 'hidden'}))
+    city = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'autocomplete': 'off'}))
+    city_id = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'type': 'hidden'}))
     class Meta:
         model = Classified
         fields = (
@@ -24,8 +24,8 @@ class AddClassifiedForm(ModelForm):
         self.fields['category'].empty_label = 'Please select a category'
 
 class EditProfileForm(ModelForm):
-    contact_city = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'autocomplete': 'off'}))
-    contact_city_id = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'type': 'hidden'}))
+    city = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'autocomplete': 'off'}))
+    city_id = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {'type': 'hidden'}))
     class Meta:
         model = UserProfile
         fields = ('username', 'email', 'phone', 'first_name', 'last_name', )
