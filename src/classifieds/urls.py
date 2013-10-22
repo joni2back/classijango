96a72abf52f73affd90c848d2ded1147dfdd6276
joni2back <joni2back@gmail.com>
2013-10-22 09:44:08 -0300

    password reset and password change section

--- src/classifieds/urls.py
+++ src/classifieds/urls.py
@@ -16,18 +16,28 @@ urlpatterns = patterns('',
         {'document_root':settings.MEDIA_ROOT}
     ),
 
+    url(r'^$', 'main.views.index'),
+
     url(r'^api/json/location/cities', 'main.views.jsonCities'),
 
-    url(r'^$', 'main.views.index'),    
     url(r'^ads/find/', 'main.views.listClassifieds'),
     url(r'^ads/(?P<classifiedId>[0-9]{1,8})/(?P<classifiedTitle>[a-zA-Z0-9_\-\/]{0,95})$', 'main.views.viewClassified'),
     url(r'^ads/edit/(?P<classifiedId>[0-9]{1,8})$', 'main.views.editClassified'),
     url(r'^ads/delete/(?P<classifiedId>[0-9]{1,8})$', 'main.views.deleteClassified'),
     url(r'^ads/create/$', 'main.views.addClassified'),
+
     url(r'^accounts/register/$', 'main.views.registerUser'),
     url(r'^accounts/profile/$', 'main.views.myProfile'),
     url(r'^accounts/profile/classifieds$', 'main.views.myClassifieds'),
     url(r'^accounts/logout/$', 'main.views.logout'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/password/change/$', 'django.contrib.auth.views.password_change', {'template_name': 'registration/password_reset_form.html'}),
+    url(r'^accounts/password/change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'registration/password_reset_done.html'}),
+
+    url(r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset', {'post_reset_redirect' : '/accounts/password/reset/done/'}),
+    url(r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'), 
+    url(r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'post_reset_redirect' : '/accounts/password/done/'}),
+    url(r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
