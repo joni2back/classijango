0e99eccbf25ecce4c2b22b85ae3192b82f36e996
joni2back <joni2back@gmail.com>
2013-11-15 16:04:03 -0300

    request middleware

--- src/classifieds/settings.py
+++ src/classifieds/settings.py
@@ -119,6 +119,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'classifieds.middleware.LocaleMiddleware'
     # Uncomment the next line for simple clickjacking protection:
     #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
